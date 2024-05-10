8c8c587a690d5ec258d5b56f5965a7e96708ca74
n70l <165950292+n70l@users.noreply.github.com>
2024-05-10 22:33:11 +0300

    Update Reset_Pass.php

--- loginsystem/Reset_Pass.php
+++ loginsystem/Reset_Pass.php
@@ -24,7 +24,7 @@
   <!-- Title Icon -->
   <link rel="icon" href="../assets/img/fav-icon.png" />
 
-  <title>تسجيل الدخول</title>
+  <title>انشاء كلمة سر جديدة</title>
   <link href="../assets/css/navbar.css" rel="stylesheet">
 
 
@@ -67,47 +67,6 @@ border: 0px solid #fff;
   </style>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>تجريبي</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
-
-<body> 
-
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light rounded" aria-label="Eleventh navbar example">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">تجريبي</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarsExample09">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">الرئيسية</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">الروابط</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">مغلق</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="dropdown09" data-bs-toggle="dropdown" aria-expanded="false">اكثر</a>
-          <ul class="dropdown-menu" aria-labelledby="dropdown09">
-            <li><a class="dropdown-item" href="#">واحد</a></li>
-            <li><a class="dropdown-item" href="#">اثنين</a></li>
-            <li><a class="dropdown-item" href="#">ثلاث</a></li>
-          </ul>
-        </li>
-      </ul>        <form>
-          <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-        </form>
-        <a href="../index.php"><button type="submit"  class="btn btn-primary llcol">تسجيل الدخول</button></a> 
-    </div>
-  </div>
-</nav>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
